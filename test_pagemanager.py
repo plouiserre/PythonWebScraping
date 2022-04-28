5ba2aac3f6bd0336a2abde8a89440bb2fb987bc3
Pierre-Louis Serré <plouiserre@gmail.com>
2022-04-28 14:12:06 +0200

    Get all links validates

--- test_pagemanager.py
+++ test_pagemanager.py
@@ -3,20 +3,53 @@ import mock
 from RequestsHelper import RequestsHelper
 from PageManager import PageManager
 
+#TODO factoriser TUs
 class PageManagerTest(unittest.TestCase):
     
     def test_html_page(self) : 
         html = "<html><h1>Big title of the page</h1></html>"
-        mockHtml =  type('',(object,),{"text":html })() 
         url = "http://www.jeuxvideo.com"
-        helper = RequestsHelper()
-        helper.Get = mock.MagicMock(return_value = mockHtml)
-        pageManager = PageManager(url, helper)
+        pageManager = self.init_pageManager_unittest(html, url)
         
-        pageManager.DownloadHtml()
+        pageManager.SearchLinks()
 
         self.assertEqual(pageManager.Url, url)
         self.assertEqual(pageManager.ContentPage, html)
+
+
+    def test_get_links (self) :
+        html = "<a class=\"stretched-link card__link\" href=\"/test/1566617/motogp-2022-le-jeu-video-de-moto-accelere-mais-pas-a-fond.htm\">MotoGP 2022 : le jeu vidéo de moto accélère, mais pas à fond !</a>                     <a class=\"stretched-link card__link\" href=\"/test/1564807/mlb-the-show-22-le-jeu-video-de-baseball-de-sony-dispo-sur-xbox-toujours-aussi-solide.htm\">MLB The Show 22 : Le jeu vidéo de Baseball de Sony dispo sur Xbox toujours aussi solide</a></h3>"
+        url = "http://www.jeuxvideo.com"
+        pageManager = self.init_pageManager_unittest(html, url)
+        
+        pageManager.SearchLinks()
+
+        firstLinks = "/test/1566617/motogp-2022-le-jeu-video-de-moto-accelere-mais-pas-a-fond.htm"
+        secondLinks = "/test/1564807/mlb-the-show-22-le-jeu-video-de-baseball-de-sony-dispo-sur-xbox-toujours-aussi-solide.htm"
+        self.assertEqual(len(pageManager.Links), 2)
+        self.assertEqual(pageManager.Links[0], firstLinks)
+        self.assertEqual(pageManager.Links[1], secondLinks)
+
+
+    def test_validLinks (self) :
+        html = "<a class=\"stretched-link card__link\" href=\"/test/1566617/motogp-2022-le-jeu-video-de-moto-accelere-mais-pas-a-fond.htm\">MotoGP 2022 : le jeu vidéo de moto accélère, mais pas à fond !</a> <a class=\"stretched-link card__link\" href=\"https://sdk.privacy-center.org/\">Link1</a><a class=\"stretched-link card__link\" href=\"/favicon.png\">Link2</a><a class=\"stretched-link card__link\" href=\"/manifest.json\">Link3</a><a class=\"stretched-link card__link\" href=\"https://static.jvc.gg/22.6.1/css/skin-common.css\">Link4</a><a class=\"stretched-link card__link\" href=\"https://static.jvc.gg/22.6.1/js/hp.js\">Link5</a></h3>"
+        url = "http://www.jeuxvideo.com"
+        pageManager = self.init_pageManager_unittest(html, url)
+        
+        pageManager.SearchLinks()
+
+        firstLinks = "/test/1566617/motogp-2022-le-jeu-video-de-moto-accelere-mais-pas-a-fond.htm"
+        self.assertEqual(len(pageManager.Links), 1)
+        self.assertEqual(pageManager.Links[0], firstLinks)
+        
+
+    def init_pageManager_unittest(self, html, url) :
+        mockHtml =  type('',(object,),{"text":html })() 
+        helper = RequestsHelper()
+        helper.Get = mock.MagicMock(return_value = mockHtml)
+        pageManager = PageManager(url, helper)
+        return pageManager
+        
 
     if __name__ == "__main__":
         unittest.main()
